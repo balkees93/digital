16542e147fca7ec2613ed36efa6fc54ba31e85b2
balkees93 <balkees.mohamed@yahoo.com>
2024-07-04 18:25:49 +0200

    js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,9 @@ function sendMail() {
   document.getElementById("myForm").addEventListener(
     "submit",
     function (event) {
-      event.preventDefault(); 
+      event.preventDefault();
       if (this.checkValidity() === false) {
-        event.stopPropagation(); 
+        event.stopPropagation();
       }
       this.classList.add("was-validated");
     },
@@ -13,3 +13,50 @@ function sendMail() {
     false
   );
 }
+
+function digiProgBar() {
+  var digiProgBar = document.getElementById("digiProgBar");
+  let seoProgBar = document.getElementById("seoProgBar");
+  let designProgBar = document.getElementById("designProgBar");
+  setTimeout(frame(), 10000);
+  function frame() {
+    if (digiProgBarWidth <= 85 || seoProgBarWidth <= 90 || designProgBarWidth <= 95) {
+      clearTimeout();
+    } else {
+      for (var digiProgBarWidth = 0; digiProgBarWidth <= 85; digiProgBarWidth++) {
+        console.log("here");
+        digiProgBar.style.width = digiProgBarWidth + "%";
+      }
+      for (var seoProgBarWidth = 0; seoProgBarWidth <= 90; seoProgBarWidth++) {
+        seoProgBar.style.width = seoProgBarWidth + "%";
+      }
+      for (var designProgBarWidth = 0; designProgBarWidth <= 95; designProgBarWidth++) {
+        designProgBar.style.width = designProgBarWidth + "%";
+      }
+    }
+  }
+}
+
+digiProgBar();
+
+function numCounter() {
+  var numberCount = document.getElementById("numberCount");
+}
+numCounter();
+$(".numberCount").each(function () {
+  $(this)
+    .prop("Counter", 0)
+    .animate(
+      {
+        Counter: $(this).text(),
+      },
+      {
+        //chnage count up speed here
+        duration: 4000,
+        easing: "swing",
+        step: function (now) {
+          $(this).text(Math.ceil(now));
+        },
+      }
+    );
+});
